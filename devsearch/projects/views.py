d3da36f614ed786e327b9dd2a94fea7ee94a5e56
cwjki <cwjki97@gmail.com>
2022-03-18 23:37:14 -0400

    finish the templates tutorials

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,10 +1,39 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+projectList = [
+  {
+    'id': '1',
+    'title': 'Ecommerce Website',
+    'description': 'Fully functional blah'    
+  },
+  {
+      'id': '2',
+      'title': 'Portafolio Website',
+      'description': 'A personal blah'
+  },
+  {
+      'id': '3',
+      'title': 'Social Network',
+      'description': 'An open source blah'
+  },
+]
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    name = 'Dennis Ivanov'
+
+    context = {'projects': projectList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return HttpResponse(f'Project page: {pk}')
+    projectObject = None
+    for i in projectList:
+      if i['id'] == str(pk):
+        projectObject = i
+    
+    context = {'project': projectObject}
+    return render(request, 'projects/singleProject.html', context)
+  
