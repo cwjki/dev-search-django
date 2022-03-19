2f3536e5c5832d7bf12d3217b36ec8dcf039f1f2
cwjki <cwjki97@gmail.com>
2022-03-18 20:48:19 -0400

    adding templates

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'devsearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/home/cwjki/Projects/django-bootcamp/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
