4a043fdf133d75b1a1a19dbd9c4c0f56a82e877b
cwjki <cwjki97@gmail.com>
2022-03-19 15:15:57 -0400

    addign some models

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Project(models.Model):
+    # owner =
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # feature_image =
+    demo_link = models.CharField(max_length=1000, null=True, blank=True)
+    source_link = models.CharField(max_length=1000, null=True, blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    # owner =
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50)
